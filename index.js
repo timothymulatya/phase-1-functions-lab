e66a18e5eb0497f33e43f11d7bbd058327e4318b
timothymulatya <timothymulatya@gmail.com>
2025-10-05 13:34:43 +0300

    Distance in Feet

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ function distanceFromHqInBlocks(pickUpStationBlock) {
   const locationOfHqBlock = 42;
   return Math.abs(locationOfHqBlock - pickUpStationBlock);
 }
+
+function distanceFromHqInFeet(pickUpBlock) {
+  const distancePerBlockInFeet = 264;
+  return distanceFromHqInBlocks(pickUpBlock) * distancePerBlockInFeet;
+}
+
 console.log(distanceFromHqInBlocks(43)); 
 console.log(distanceFromHqInBlocks(50)); 
 console.log(distanceFromHqInBlocks(34)); 
+
+console.log(distanceFromHqInFeet(43)); 
+console.log(distanceFromHqInFeet(50)); 
+console.log(distanceFromHqInFeet(34)); 
