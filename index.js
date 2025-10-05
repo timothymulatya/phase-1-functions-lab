41534d9b4f91fcf38305392fc8e9a8e905888d4e
timothymulatya <timothymulatya@gmail.com>
2025-10-05 13:12:47 +0300

    Distance in Blocks

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const { startTransition } = require("react")
+
 // Code your solution in this file!
+// distanceFromHqInBlocks(42 - pickUpStation)
+// distanceFromHqInFeet(distanceFromHqInBlocks * 264)
+// distanceTravelledInFeet( start, destination)
+// calculatesFarePrice( distanceTravelledInFeet * farePrice)
+
+function distanceFromHqInBlocks(pickUpStationBlock) {
+  const locationOfHqBlock = 42;
+  return Math.abs(locationOfHqBlock - pickUpStationBlock);
+}
+console.log(distanceFromHqInBlocks(43)); 
+console.log(distanceFromHqInBlocks(50)); 
+console.log(distanceFromHqInBlocks(34)); 
