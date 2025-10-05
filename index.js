549f25b25107dbbe28d163984c3ed4273e80636e
timothymulatya <timothymulatya@gmail.com>
2025-10-05 13:51:48 +0300

    Calculate Fare Price

--- index.js
+++ index.js
@@ -20,6 +20,18 @@ function distanceTravelledInFeet(start, destination) {
   const distancePerBlockInFeet = 264;
   return Math.abs(destination - start) * distancePerBlockInFeet;
 }
+function calculatesFarePrice(pickUpBlock, destination) {
+    const distance = distanceTravelledInFeet(pickUpBlock, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
 
 console.log(distanceFromHqInBlocks(43)); 
 console.log(distanceFromHqInBlocks(50)); 
@@ -32,3 +44,7 @@ console.log(distanceFromHqInFeet(34));
 console.log(distanceTravelledInFeet(43));
 console.log(distanceTravelledInFeet(50));
 console.log(distanceTravelledInFeet(34));
+
+console.log(calculatesFarePrice(43));
+console.log(calculatesFarePrice(50));
+console.log(calculatesFarePrice(34));
